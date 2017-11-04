b3caf721f5d8d7785f0490644296ffd7fa49fe5f
Rajesh Karmaker <rajeshkarmaker0@gmail.com>
2017-11-04 23:11:07 +0600

    First Commit

--- WebServiceExample/ViewController.m
+++ WebServiceExample/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+static NSString *api_key = @"AIzaSyBq9R7SSLKp9CeyhGPU6WM8y96qjJbLdao";
 @interface ViewController ()
 
 @end
@@ -16,10 +16,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self makePOSTReguest];
 }
-
-
+-(void) makeGETReguest1{
+    NSString *urlString = [NSString stringWithFormat:@"https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=%@",api_key];
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:url];
+    [request setHTTPMethod:@"GET"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    NSURLSession *session  = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSHTTPURLResponse *urlRes = (NSHTTPURLResponse *) response;
+        if(urlRes.statusCode == 200){
+            NSError *err = nil;
+            NSDictionary *responseDic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&err];
+            NSLog(@"%@",responseDic);
+        }
+    }];
+    [dataTask resume];
+}
+-(void) makeGETReguest2{
+    NSString *urlString = @"http://192.168.0.100:1234/luka_api/read.php";
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:url];
+    [request setHTTPMethod:@"GET"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    NSURLSession *session  = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSHTTPURLResponse *urlRes = (NSHTTPURLResponse *) response;
+        if(urlRes.statusCode == 200){
+            NSError *err = nil;
+            NSDictionary *responseDic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&err];
+            NSLog(@"%@",responseDic);
+        }
+    }];
+    [dataTask resume];
+}
+-(void)makePOSTReguest{
+    NSString *urlString = @"http://192.168.0.100:1234/luka_api/create.php";
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+    [dict setObject:@"From iOS" forKey:@"title"];
+    [dict setObject:[NSNumber numberWithInt:104] forKey:@"userID"];
+    [dict setObject:@"poll" forKey:@"type"];
+    NSError *error;
+    NSData *postdata = [NSJSONSerialization dataWithJSONObject:dict options:0 error:&error];
+    [request setHTTPBody:postdata];
+    NSURLSession *session  = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSHTTPURLResponse *urlRes = (NSHTTPURLResponse *) response;
+        if(urlRes.statusCode == 200){
+            
+            NSLog(@"%@",response);
+        }
+    }];
+    [dataTask resume];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
